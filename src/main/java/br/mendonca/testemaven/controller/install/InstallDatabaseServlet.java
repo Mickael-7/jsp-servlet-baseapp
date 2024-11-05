8582851e169c447470efcfd2b2bfd06357923457
Mickael-7 <mickael.albuquerque@upe.br>
2024-11-05 16:45:33 -0300

    feat(Database): criar tabela de exercicio

--- src/main/java/br/mendonca/testemaven/controller/install/InstallDatabaseServlet.java
+++ src/main/java/br/mendonca/testemaven/controller/install/InstallDatabaseServlet.java
@@ -31,6 +31,9 @@ public class InstallDatabaseServlet extends HttpServlet {
 			
 			service.createUserTable();
 			msg += "<h2>Create table user sucessful!</h2>\n";
+
+			service.createExercicioTable();
+			msg += "<h2>Create table exercicio sucessful!</h2>\n";
 			
 			page.println("<html lang='pt-br'><head><title>Teste</title></head><body>");
 			page.println(msg);
@@ -46,8 +49,8 @@ public class InstallDatabaseServlet extends HttpServlet {
 			page.close();
 			
 		} catch (Exception e) {
-			// Escreve as mensagens de Exception em uma página de resposta.
-			// Não apagar este bloco.
+			// Escreve as mensagens de Exception em uma pï¿½gina de resposta.
+			// Nï¿½o apagar este bloco.
 			StringWriter sw = new StringWriter();
 			PrintWriter pw = new PrintWriter(sw);
 			e.printStackTrace(pw);
