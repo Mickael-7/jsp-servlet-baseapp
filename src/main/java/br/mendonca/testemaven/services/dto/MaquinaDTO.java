565d5029a8805cd400a5b80a8c2ff261a9fd6693
igoreor <igoreor24@gmail.com>
2024-11-05 16:07:54 -0300

    feat(maquina): adiciar o maquina DTO

---
+++ src/main/java/br/mendonca/testemaven/services/dto/MaquinaDTO.java
@@ -0,0 +1,44 @@
+package br.mendonca.testemaven.services.dto;
+
+import br.mendonca.testemaven.model.entities.Maquina;
+
+public class MaquinaDTO {
+
+    private String nome;
+    private float pesoTotal;
+    private boolean quebrada;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPesoTotal() {
+        return pesoTotal;
+    }
+
+    public void setPesoTotal(float pesoTotal) {
+        this.pesoTotal = pesoTotal;
+    }
+
+    public boolean isQuebrada() {
+        return quebrada;
+    }
+
+    public void setQuebrada(boolean quebrada) {
+        this.quebrada = quebrada;
+    }
+
+    public static MaquinaDTO maquinaMapper(Maquina maquina) {
+        MaquinaDTO dto = new MaquinaDTO();
+        dto.setNome(maquina.getNome());
+        dto.setPesoTotal(maquina.getPesoTotal());
+        dto.setQuebrada(maquina.isQuebrada());
+
+        return dto;
+    }
+}
