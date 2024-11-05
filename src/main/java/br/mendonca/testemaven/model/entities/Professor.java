7a8b11ad04e0de7a58250ea46cb0b57c338cd7df
NathanDalbert <dalbert.nathan@gmail.com>
2024-11-05 14:42:27 -0300

    feat(Professor): Criar entidade professor

---
+++ src/main/java/br/mendonca/testemaven/model/entities/Professor.java
@@ -0,0 +1,40 @@
+package br.mendonca.testemaven.model.entities;
+
+public class Professor {
+    private String uuid;
+    private String name;
+    private Integer idade;
+    private boolean estaPresente;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public boolean isEstaPresente() {
+        return estaPresente;
+    }
+
+    public void setEstaPresente(boolean estaPresente) {
+        this.estaPresente = estaPresente;
+    }
+}
