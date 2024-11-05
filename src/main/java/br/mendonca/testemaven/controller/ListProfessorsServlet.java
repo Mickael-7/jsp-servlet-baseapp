d80bf0142c115ae474d4f6e56af820e0d1702a84
NathanDalbert <dalbert.nathan@gmail.com>
2024-11-05 17:18:51 -0300

    feat(ListProfessor):Criar ListProfessor

---
+++ src/main/java/br/mendonca/testemaven/controller/ListProfessorsServlet.java
@@ -0,0 +1,69 @@
+package br.mendonca.testemaven.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+
+import br.mendonca.testemaven.services.ProfessorService;
+import br.mendonca.testemaven.services.dto.ProfessorDTO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/dashboard/professors")
+public class ListProfessorsServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter page = response.getWriter();
+
+        try {
+            ProfessorService service = new ProfessorService();
+            List<ProfessorDTO> lista = service.listAllProfessors();
+
+            request.setAttribute("lista", lista);
+            request.getRequestDispatcher("list-professors.jsp").forward(request, response);
+        } catch (Exception e) {
+
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+
+            page.println("<html lang='pt-br'><head><title>Error</title></head><body>");
+            page.println("<h1>Error</h1>");
+            page.println("<code>" + sw.toString() + "</code>");
+            page.println("</body></html>");
+            page.close();
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter page = response.getWriter();
+
+        try {
+
+            String parametro = request.getParameter("nomeparametro");
+
+            page.println("Parametro: " + parametro);
+            page.close();
+
+        } catch (Exception e) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+
+            page.println("<html lang='pt-br'><head><title>Error</title></head><body>");
+            page.println("<h1>Error</h1>");
+            page.println("<code>");
+            page.println(sw.toString());
+            page.println("</code>");
+            page.println("</body></html>");
+            page.close();
+        }
+    }
+}
