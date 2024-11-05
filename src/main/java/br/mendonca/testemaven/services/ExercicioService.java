b12ff7a3492343d9b170a0eaa85738a0b376eae8
Mickael-7 <mickael.albuquerque@upe.br>
2024-11-05 16:53:23 -0300

    feat(ExercicioServlet): criar ExercicioServlet

---
+++ src/main/java/br/mendonca/testemaven/services/ExercicioService.java
@@ -0,0 +1,36 @@
+package br.mendonca.testemaven.services;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.mendonca.testemaven.dao.ExercicioDAO;
+import br.mendonca.testemaven.model.entities.Exercicio;
+import br.mendonca.testemaven.services.dto.ExercicioDTO;
+
+public class ExercicioService {
+
+    public void register(String nome, int quantidadeSeries, boolean disponivel) throws SQLException, ClassNotFoundException {
+        ExercicioDAO dao = new ExercicioDAO();
+        Exercicio exercicio = new Exercicio();
+        exercicio.setNome(nome);
+        exercicio.setQuantidadeSeries(quantidadeSeries);
+        exercicio.setDisponivelNaAcademia(disponivel);
+
+        dao.register(exercicio); // Supondo que você tenha um método register no DAO
+    }
+
+    public List<ExercicioDTO> listAllExercicios() throws ClassNotFoundException, SQLException {
+        List<ExercicioDTO> resp = new ArrayList<>();
+
+        ExercicioDAO dao = new ExercicioDAO();
+        List<Exercicio> lista = dao.listAllExercicios();
+
+        for (Exercicio exercicio : lista) {
+            resp.add(ExercicioDTO.exercicioMapper(exercicio));
+        }
+
+        return resp;
+    }
+
+}
